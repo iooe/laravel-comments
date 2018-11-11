ed6109d356edbed2fe145eb7370f5200b92a738d
Roman Epanchenko <podstavat@gmail.com>
2018-11-11 18:01:09 +0600

    Adding auth except for get method

--- src/Http/Controllers/CommentsController.php
+++ src/Http/Controllers/CommentsController.php
@@ -19,7 +19,7 @@ class CommentsController extends Controller
 
     public function __construct()
     {
-        $this->middleware(['web', 'auth']);
+        $this->middleware(['web', 'auth'], ['except' => ['get']]);
     }
 
     /**
