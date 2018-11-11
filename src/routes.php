a8b4f71323e1a2fe875f342badc22024bf6626d7
Roman Epanchenko <podstavat@gmail.com>
2018-11-11 18:01:33 +0600

    Move routes to api group

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,15 @@
 <?php
 
-Route::get('comments', '\tizis\laraComments\Http\Controllers\CommentsController@get');
-Route::post('comments', '\tizis\laraComments\Http\Controllers\CommentsController@store');
-Route::delete('comments/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@destroy');
-Route::put('comments/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@update');
-Route::post('comments/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@reply');
+Route::group(['prefix' => 'api'], function () {
+    /**
+     * Comments
+     */
+    Route::group(['prefix' => 'comments'], function () {
+        Route::get('/', '\tizis\laraComments\Http\Controllers\CommentsController@get');
+        Route::post('/', '\tizis\laraComments\Http\Controllers\CommentsController@store');
+        Route::delete('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@destroy');
+        Route::put('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@update');
+        Route::post('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@reply');
+    });
+});
+
