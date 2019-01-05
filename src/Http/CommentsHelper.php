4a8af71d36577f9368337707182101356fcb7f64
Roman Epanchenko <podstavat@gmail.com>
2019-01-05 18:23:17 +0600

    Update CommentsHelper.php

--- src/Http/CommentsHelper.php
+++ src/Http/CommentsHelper.php
@@ -7,6 +7,12 @@ use tizis\laraComments\UseCases\CommentService;
 
 class CommentsHelper
 {
+    /**
+     * Alias to CommentService::getNewestComments
+     * @param int $take
+     * @param null $commentable_type
+     * @return mixed
+     */
     public static function getNewestComments($take = 10, $commentable_type = null)
     {
         return CommentResource::collection(CommentService::getNewestComments($take, $commentable_type));
