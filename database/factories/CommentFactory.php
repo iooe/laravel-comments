4e11487464e6b327aa9b5d68ad0c7d275cb1e3cf
Roman Yepanchenko <podstavat@gmail.com>
2019-06-13 12:16:58 +0600

    Update CommentFactory.php

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,31 +1,30 @@
 <?php
 
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
 use Faker\Generator as Faker;
 
-
-use App\Entity\Ranobe;
-use App\Entity\User;
-use App\Entity\Comment;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(\App\Entity\Comment::class, function (Faker $faker) {
 
-        $post = Ranobe::orderByRaw('RAND()')->first();
-        $user = User::orderByRaw('RAND()')->first();
+    $post = Post::orderByRaw('RAND()')->first();
+    $user = User::orderByRaw('RAND()')->first();
 
-        $commentableId = $post->id;
-        $childId = null;
-        if (random_int(0, 1)) {
-            $parentComment = Comment::orderByRaw('RAND()')->first();
-            $childId = $parentComment->id;
-            $commentableId = $parentComment->commentable_id;
-        }
-        return [
-            'commenter_id' => $user->id,
-            'commentable_type' => App\Entity\Ranobe::class,
-            'commentable_id' => $commentableId,
-            'comment' => $faker->text,
-            'child_id' => $childId,
-        ];
+    $commentableId = $post->id;
+    $childId = null;
+    if (random_int(0, 1)) {
+        $parentComment = Comment::orderByRaw('RAND()')->first();
+        $childId = $parentComment->id;
+        $commentableId = $parentComment->commentable_id;
+    }
+    return [
+        'commenter_id' => $user->id,
+        'commentable_type' => Post::class,
+        'commentable_id' => $commentableId,
+        'comment' => $faker->text,
+        'child_id' => $childId,
+    ];
 });
