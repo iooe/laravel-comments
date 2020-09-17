36b782a9e8e2ff615c7c9f0e5b73e86cbe1841da
Roman Yepanchenko <podstavat@gmail.com>
2020-09-17 21:55:27 +0600

    default routes refactoring

--- src/routes.php
+++ src/routes.php
@@ -1,18 +1,18 @@
 <?php
 
+use tizis\laraComments\Http\Controllers\CommentsController;
+use tizis\laraComments\Http\Controllers\VoteController;
+
 if (config('comments.route.root') !== null) {
     Route::group(['prefix' => config('comments.route.root')], static function () {
-        /**
-         * Comments
-         */
         Route::group(['prefix' => config('comments.route.group'), 'as' => 'comments.',], static function () {
-            Route::get('/', '\tizis\laraComments\Http\Controllers\CommentsController@get')->name('get');
-            Route::post('/', '\tizis\laraComments\Http\Controllers\CommentsController@store')->name('store');
-            Route::delete('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@destroy')->name('delete');
-            Route::put('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@update')->name('update');
-            Route::get('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@show');
-            Route::post('/{comment}', '\tizis\laraComments\Http\Controllers\CommentsController@reply')->name('reply');
-            Route::post('/{comment}/vote', '\tizis\laraComments\Http\Controllers\VoteController@vote')->name('vote');
+            Route::get('/', [CommentsController::class, 'get'])->name('get');
+            Route::post('/', [CommentsController::class, 'store'])->name('store');
+            Route::delete('/{comment}', [CommentsController::class, 'destroy'])->name('delete');
+            Route::put('/{comment}', [CommentsController::class, 'update'])->name('update');
+            Route::get('/{comment}', [CommentsController::class, 'show']);
+            Route::post('/{comment}', [CommentsController::class, 'reply'])->name('reply');
+            Route::post('/{comment}/vote', [VoteController::class, 'vote'])->name('vote');
         });
     });
 }
